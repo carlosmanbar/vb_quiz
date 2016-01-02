7f27c2de28d58e13fa61928ae175af340415c970
Carlos Barbosa <carlosmanbar@gmail.com>
2016-01-02 14:36:53 +0000

    project update
    upload of the project as it was

---
+++ Form1.Designer.vb
@@ -0,0 +1,217 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.StatusStrip = New System.Windows.Forms.StatusStrip()
+        Me.ltTime = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.tlPoints = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.ltDifficulty = New System.Windows.Forms.ToolStripStatusLabel()
+        Me.MenuStrip1 = New System.Windows.Forms.MenuStrip()
+        Me.MenuToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.NovoToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.AjudaToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SairToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.lQuestion = New System.Windows.Forms.Label()
+        Me.btAnswer1 = New System.Windows.Forms.Button()
+        Me.btAnswer2 = New System.Windows.Forms.Button()
+        Me.btAnswer3 = New System.Windows.Forms.Button()
+        Me.btAnswer4 = New System.Windows.Forms.Button()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.StatusStrip.SuspendLayout()
+        Me.MenuStrip1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'StatusStrip
+        '
+        Me.StatusStrip.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ltTime, Me.tlPoints, Me.ltDifficulty})
+        Me.StatusStrip.Location = New System.Drawing.Point(0, 337)
+        Me.StatusStrip.Name = "StatusStrip"
+        Me.StatusStrip.RightToLeft = System.Windows.Forms.RightToLeft.Yes
+        Me.StatusStrip.Size = New System.Drawing.Size(284, 24)
+        Me.StatusStrip.SizingGrip = False
+        Me.StatusStrip.TabIndex = 0
+        Me.StatusStrip.Text = "StatusStrip1"
+        '
+        'ltTime
+        '
+        Me.ltTime.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left
+        Me.ltTime.Name = "ltTime"
+        Me.ltTime.Size = New System.Drawing.Size(50, 19)
+        Me.ltTime.Text = "Time: 1"
+        '
+        'tlPoints
+        '
+        Me.tlPoints.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left
+        Me.tlPoints.Name = "tlPoints"
+        Me.tlPoints.Size = New System.Drawing.Size(68, 19)
+        Me.tlPoints.Text = "Points: 100"
+        '
+        'ltDifficulty
+        '
+        Me.ltDifficulty.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Left
+        Me.ltDifficulty.Name = "ltDifficulty"
+        Me.ltDifficulty.Size = New System.Drawing.Size(88, 19)
+        Me.ltDifficulty.Text = "Difficulty: Easy"
+        '
+        'MenuStrip1
+        '
+        Me.MenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.MenuToolStripMenuItem})
+        Me.MenuStrip1.Location = New System.Drawing.Point(0, 0)
+        Me.MenuStrip1.Name = "MenuStrip1"
+        Me.MenuStrip1.Size = New System.Drawing.Size(284, 24)
+        Me.MenuStrip1.TabIndex = 1
+        Me.MenuStrip1.Text = "MenuStrip1"
+        '
+        'MenuToolStripMenuItem
+        '
+        Me.MenuToolStripMenuItem.DropDownItems.AddRange(New System.Windows.Forms.ToolStripItem() {Me.NovoToolStripMenuItem, Me.AjudaToolStripMenuItem, Me.SairToolStripMenuItem})
+        Me.MenuToolStripMenuItem.Name = "MenuToolStripMenuItem"
+        Me.MenuToolStripMenuItem.Size = New System.Drawing.Size(50, 20)
+        Me.MenuToolStripMenuItem.Text = "&Menu"
+        '
+        'NovoToolStripMenuItem
+        '
+        Me.NovoToolStripMenuItem.Name = "NovoToolStripMenuItem"
+        Me.NovoToolStripMenuItem.Size = New System.Drawing.Size(131, 22)
+        Me.NovoToolStripMenuItem.Text = "&New game"
+        '
+        'AjudaToolStripMenuItem
+        '
+        Me.AjudaToolStripMenuItem.Name = "AjudaToolStripMenuItem"
+        Me.AjudaToolStripMenuItem.Size = New System.Drawing.Size(131, 22)
+        Me.AjudaToolStripMenuItem.Text = "&About"
+        '
+        'SairToolStripMenuItem
+        '
+        Me.SairToolStripMenuItem.Name = "SairToolStripMenuItem"
+        Me.SairToolStripMenuItem.Size = New System.Drawing.Size(131, 22)
+        Me.SairToolStripMenuItem.Text = "&Exit"
+        '
+        'lQuestion
+        '
+        Me.lQuestion.Font = New System.Drawing.Font("Verdana", 12.0!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lQuestion.Location = New System.Drawing.Point(12, 37)
+        Me.lQuestion.Name = "lQuestion"
+        Me.lQuestion.Size = New System.Drawing.Size(260, 74)
+        Me.lQuestion.TabIndex = 2
+        Me.lQuestion.Text = "Pergunta qualquer coisa se for grande demais ou ainda maior, tipo mesmo grande da" &
+    "rá para mais quatro linhas"
+        Me.lQuestion.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        Me.lQuestion.Visible = False
+        '
+        'btAnswer1
+        '
+        Me.btAnswer1.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btAnswer1.Location = New System.Drawing.Point(12, 130)
+        Me.btAnswer1.Name = "btAnswer1"
+        Me.btAnswer1.Size = New System.Drawing.Size(260, 44)
+        Me.btAnswer1.TabIndex = 3
+        Me.btAnswer1.Text = "Resposta fixe que pode conter duas linhas"
+        Me.btAnswer1.UseVisualStyleBackColor = True
+        Me.btAnswer1.Visible = False
+        '
+        'btAnswer2
+        '
+        Me.btAnswer2.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btAnswer2.Location = New System.Drawing.Point(12, 180)
+        Me.btAnswer2.Name = "btAnswer2"
+        Me.btAnswer2.Size = New System.Drawing.Size(260, 44)
+        Me.btAnswer2.TabIndex = 4
+        Me.btAnswer2.Text = "Resposta fixe que pode conter duas linhas"
+        Me.btAnswer2.UseVisualStyleBackColor = True
+        Me.btAnswer2.Visible = False
+        '
+        'btAnswer3
+        '
+        Me.btAnswer3.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btAnswer3.Location = New System.Drawing.Point(12, 230)
+        Me.btAnswer3.Name = "btAnswer3"
+        Me.btAnswer3.Size = New System.Drawing.Size(260, 44)
+        Me.btAnswer3.TabIndex = 5
+        Me.btAnswer3.Text = "Resposta fixe que pode conter duas linhas"
+        Me.btAnswer3.UseVisualStyleBackColor = True
+        Me.btAnswer3.Visible = False
+        '
+        'btAnswer4
+        '
+        Me.btAnswer4.Font = New System.Drawing.Font("Verdana", 9.75!, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btAnswer4.Location = New System.Drawing.Point(12, 280)
+        Me.btAnswer4.Name = "btAnswer4"
+        Me.btAnswer4.Size = New System.Drawing.Size(260, 44)
+        Me.btAnswer4.TabIndex = 6
+        Me.btAnswer4.Text = "Resposta fixe que pode conter duas linhas"
+        Me.btAnswer4.UseVisualStyleBackColor = True
+        Me.btAnswer4.Visible = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 1000
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = Global.cQuiz.My.Resources.Resources.backwelcome
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(284, 361)
+        Me.Controls.Add(Me.btAnswer4)
+        Me.Controls.Add(Me.btAnswer3)
+        Me.Controls.Add(Me.btAnswer2)
+        Me.Controls.Add(Me.btAnswer1)
+        Me.Controls.Add(Me.lQuestion)
+        Me.Controls.Add(Me.StatusStrip)
+        Me.Controls.Add(Me.MenuStrip1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.MainMenuStrip = Me.MenuStrip1
+        Me.MaximizeBox = False
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "cQuiz"
+        Me.StatusStrip.ResumeLayout(False)
+        Me.StatusStrip.PerformLayout()
+        Me.MenuStrip1.ResumeLayout(False)
+        Me.MenuStrip1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents StatusStrip As StatusStrip
+    Friend WithEvents MenuStrip1 As MenuStrip
+    Friend WithEvents ltTime As ToolStripStatusLabel
+    Friend WithEvents tlPoints As ToolStripStatusLabel
+    Friend WithEvents MenuToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents NovoToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents AjudaToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents SairToolStripMenuItem As ToolStripMenuItem
+    Friend WithEvents ltDifficulty As ToolStripStatusLabel
+    Friend WithEvents lQuestion As Label
+    Friend WithEvents btAnswer1 As Button
+    Friend WithEvents btAnswer2 As Button
+    Friend WithEvents btAnswer3 As Button
+    Friend WithEvents btAnswer4 As Button
+    Friend WithEvents Timer1 As Timer
+End Class
---
+++ Form1.vb
@@ -0,0 +1,331 @@
+﻿Imports System.IO
+Public Class Form1
+
+
+    'Notes TODO
+    'Add difficulty (easy, medium, hard, extreme)
+    'Read questions from a txt file
+    'add new questions and write them to the txt file
+
+
+    'Read and wright text to question.txt
+    'vetores para o texto
+    Dim quest(1)
+    Dim um(1)
+    Dim dois(1)
+    Dim tres(1)
+    Dim quatro(1)
+    Dim correto(1)
+
+
+
+    'Global vars
+    Dim gameTimer, gamePoints, gameDifficulty
+    Dim level
+
+    'tempo maximo de jogo
+    Dim totalTime = 100
+    'pontos minimos de jogo
+    Dim minPoints = -20
+
+    'For the game
+    Dim answer, question
+    'Dim question = 1
+
+
+
+
+
+    Private Sub NovoToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles NovoToolStripMenuItem.Click
+        'Start new game
+        'start the gamequiz
+        'with the 1st question
+        question = 1
+        startGame()
+
+    End Sub
+
+    Private Sub AjudaToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles AjudaToolStripMenuItem.Click
+        'Call About Form
+        Form2.Show()
+    End Sub
+
+    Private Sub SairToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SairToolStripMenuItem.Click
+        'Exit game
+        Close()
+    End Sub
+
+
+
+    Private Sub Form1_Closed(sender As Object, e As EventArgs) Handles Me.Closed
+        'Exit message
+        MessageBox.Show("Thank you for playing!")
+    End Sub
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        'Clock count
+        gameTimer += 1
+        ltTime.Text = "Time: " & gameTimer
+
+        'Add time limit?
+        endGame()
+    End Sub
+
+    Function gameQuiz(q)
+        Select Case q
+            Case 1
+                lQuestion.Text = "First question is something cool!"
+                btAnswer1.Text = "First answer (T)"
+                btAnswer2.Text = "Second answer"
+                btAnswer3.Text = "Third answer"
+                btAnswer4.Text = "Fourth answer"
+                answer = 1
+            Case 2
+                lQuestion.Text = "Second question is something cool!"
+                btAnswer1.Text = "First answer"
+                btAnswer2.Text = "Second answer(T)"
+                btAnswer3.Text = "Third answer"
+                btAnswer4.Text = "Fourth answer"
+                answer = 2
+            Case 3
+                lQuestion.Text = "Third question is something cool!"
+                btAnswer1.Text = "First answer"
+                btAnswer2.Text = "Second answer"
+                btAnswer3.Text = "Third answer(T)"
+                btAnswer4.Text = "Fourth answer"
+                answer = 3
+            Case 4
+                lQuestion.Text = "Fourth question is something cool!"
+                btAnswer1.Text = "First answer"
+                btAnswer2.Text = "Second answer"
+                btAnswer3.Text = "Third answer"
+                btAnswer4.Text = "Fourth answer(T)"
+                answer = 4
+            Case 5
+                lQuestion.Text = "Fifth question is something cool!"
+                btAnswer1.Text = "First answer(T)"
+                btAnswer2.Text = "Second answer"
+                btAnswer3.Text = "Third answer"
+                btAnswer4.Text = "Fourth answer"
+                answer = 1
+            Case 6
+                lQuestion.Text = "Sixth question is something cool!"
+                btAnswer1.Text = "First answer"
+                btAnswer2.Text = "Second answer(T)"
+                btAnswer3.Text = "Third answer"
+                btAnswer4.Text = "Fourth answer"
+                answer = 2
+            Case Else
+                lQuestion.Visible = False
+                btAnswer1.Visible = False
+                btAnswer2.Visible = False
+                btAnswer3.Visible = False
+                btAnswer4.Visible = False
+                Me.BackgroundImage = cQuiz.My.Resources.Resources.backwelcome
+                'MessageBox.Show("No more questions!")
+                Dim msg As Integer = MessageBox.Show("Wanna play again?", "No more questions!", MessageBoxButtons.YesNo)
+                If msg = DialogResult.No Then
+                    'MessageBox.Show("Thanks for playing!")
+                    Me.Close()
+                ElseIf msg = DialogResult.Yes Then
+                    MessageBox.Show("Good luck again!")
+                    startGame()
+
+                End If
+        End Select
+
+    End Function
+
+    Function points(n)
+        'function gives or takes points
+        gamePoints = gamePoints + n
+        tlPoints.Text = "Points: " & gamePoints
+    End Function
+
+    Function endGame()
+        'this will control when the game ends
+
+        'end by time?
+        If gameTimer >= totalTime Or gamePoints <= minPoints Then
+            Timer1.Stop()
+            lQuestion.Visible = False
+            btAnswer1.Visible = False
+            btAnswer2.Visible = False
+            btAnswer3.Visible = False
+            btAnswer4.Visible = False
+            Me.BackgroundImage = cQuiz.My.Resources.Resources.backgameover
+            Dim msg As Integer = MessageBox.Show("Game over, wanna play again?", "Game Over!", MessageBoxButtons.YesNo)
+            If msg = DialogResult.No Then
+                'MessageBox.Show("Thanks for playing!")
+                Me.Close()
+            ElseIf msg = DialogResult.Yes Then
+                MessageBox.Show("Good luck again!")
+                startGame()
+            End If
+        End If
+
+
+
+    End Function
+
+    Function startGame()
+        'Start new game
+
+        gameTimer = 0
+        'points status
+        '10pts for 1 correct, -5pts for 1 rong answer
+        gamePoints = 0
+
+        points(0)
+        'gameDifficulty = "Easy"
+        ltDifficulty.Visible = False 'inactive for now, see TODO list
+
+        'Change background
+        Me.BackgroundImage = cQuiz.My.Resources.Resources.back 'game background
+        'Me.BackgroundImage = cQuiz.My.Resources.Resources.backgameover 'game over
+        'Me.BackgroundImage = cQuiz.My.Resources.Resources.backwelcome 'game start
+
+        'start clock
+        Timer1.Start()
+
+        'start the gamequiz
+        'with the 1st question
+        question = 1
+        gameQuiz(question)
+
+
+        'Turn my objects visible
+        lQuestion.Visible = True
+        btAnswer1.Visible = True
+        btAnswer2.Visible = True
+        btAnswer3.Visible = True
+        btAnswer4.Visible = True
+    End Function
+
+    Private Sub btAnswer1_Click(sender As Object, e As EventArgs) Handles btAnswer1.Click
+        If answer = 1 Then
+            'correct answer
+
+            'win points
+            points(10)
+
+            'go to next question
+            question += 1
+            gameQuiz(question)
+
+
+        Else
+            'rong question
+            points(-5)
+        End If
+        'go to next question here
+        'question += 1
+    End Sub
+
+    Private Sub btAnswer2_Click(sender As Object, e As EventArgs) Handles btAnswer2.Click
+        If answer = 2 Then
+            'correct answer
+
+            'win points
+            points(10)
+
+            'go to next question
+            question += 1
+            gameQuiz(question)
+
+
+        Else
+            'rong question
+            points(-5)
+        End If
+        'go to next question here
+        'question += 1
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        read()
+        MessageBox.Show(quest(1))
+    End Sub
+
+    Private Sub btAnswer3_Click(sender As Object, e As EventArgs) Handles btAnswer3.Click
+        If answer = 3 Then
+            'correct answer
+
+            'win points
+            points(10)
+
+            'go to next question
+            question += 1
+            gameQuiz(question)
+
+
+        Else
+            'rong question
+            points(-5)
+        End If
+        'go to next question here
+        'question += 1
+    End Sub
+    Private Sub btAnswer4_Click(sender As Object, e As EventArgs) Handles btAnswer4.Click
+        If answer = 4 Then
+            'correct answer
+
+            'win points
+            points(10)
+
+            'go to next question
+            question += 1
+            gameQuiz(question)
+
+
+        Else
+            'rong question
+            points(-5)
+        End If
+        'go to next question here
+        'question += 1
+    End Sub
+
+    Function read()
+        'code copied from https://support.microsoft.com/en-us/kb/315828
+
+        Dim objStreamReader As StreamReader
+        Dim strLine As String
+
+        'Pass the file path and the file name to the StreamReader constructor.
+        objStreamReader = New StreamReader("C:\Users\va2015185\Downloads\cQuiz\cQuiz\cQuiz\questions.txt")
+
+        'Read the first line of text.
+        strLine = objStreamReader.ReadLine
+
+        'Continue to read until you reach the end of the file.
+        Do While Not strLine Is Nothing
+            quest(0) = strLine
+
+
+            'For i = 0 To quest.Length
+            '    quest(i) = strLine
+            '    um(i) = strLine
+            '    dois(i) = strLine
+            '    tres(i) = strLine
+            '    quatro(i) = strLine
+            '    correto(i) = strLine
+
+            'Next
+
+            'Write the line to the Console window.
+            'Console.WriteLine(strLine)
+            ' MessageBox.Show(strLine)
+            quest(0) = strLine
+            'Read the next line.
+            strLine = objStreamReader.ReadLine
+        Loop
+
+        'Close the file.
+        objStreamReader.Close()
+
+        'Console.ReadLine()
+        'MessageBox.Show(strLine)
+        'MessageBox.Show(quest(0))
+    End Function
+
+End Class
